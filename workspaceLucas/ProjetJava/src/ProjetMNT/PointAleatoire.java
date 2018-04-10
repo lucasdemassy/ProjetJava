c0b64442119272cb01c8af98eb48157114576dbf
lucasdemassy <38038514+lucasdemassy@users.noreply.github.com>
2018-04-10 14:18:09 +0200

    Add files via upload

--- workspaceLucas/ProjetJava/src/ProjetMNT/PointAleatoire.java
+++ workspaceLucas/ProjetJava/src/ProjetMNT/PointAleatoire.java
@@ -1,10 +1,42 @@
 package ProjetMNT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PointAleatoire extends Point{
 
 	public PointAleatoire(double x, double y, double z) {
 		super(x, y, z);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static List<Point> MNTAleatoire(double longueur, double largeur, double precision, double altitudeMin, double altitudeMax)	{
+		// Faire gaffe aux points entiers (éviter les points décimaux)
+		List<Point> grille = new ArrayList<Point>();
+		double altitude;
+		for (double i = 0; i <= largeur; i+=precision)	{
+			for (double j = 0; j <= longueur; j+=precision)	{
+				altitude = altitudeMin + Math.random()*(altitudeMax - altitudeMin);
+				grille.add(new PointAleatoire(i,j,altitude));
+			}
+		}
+		
+		/*
+		
+		double pointsColonne = nb_points / longueur;	//Nombre de points par unité de longueur
+		double pointsLigne   = nb_points / largeur;		//Nombre de points par unité de largeur
+		double pointsSurface = nb_points /(pointsColonne * pointsLigne);		//Nombre de points par unité de surface
+		double pasColonne    = 1 / pointsColonne;
+		double pasLigne      = ;
+		*/
+		return grille;
+	}
+	
+	public static void main(String[] args) {
+		List<Point> MNT1 = MNTAleatoire(20,30,1,0,100);
+		for(int i=0; i<MNT1.size(); i++)	{
+			System.out.println(MNT1.get(i).getX() + ", " + MNT1.get(i).getY() + ", " + MNT1.get(i).getZ());
+		}
 	}
 
 }
