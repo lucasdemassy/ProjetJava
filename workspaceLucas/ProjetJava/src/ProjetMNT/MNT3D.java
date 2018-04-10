59746c4a459957fccaf3997ab54ef403c40e7e41
lucasdemassy <38038514+lucasdemassy@users.noreply.github.com>
2018-04-10 12:35:55 +0200

    Add files via upload

---
+++ workspaceLucas/ProjetJava/src/ProjetMNT/MNT3D.java
@@ -0,0 +1,12 @@
+package ProjetMNT;
+
+import java.util.List;
+
+public class MNT3D extends MNT{
+
+	public MNT3D(List<Point> points) {
+		super(points);
+		// TODO Auto-generated constructor stub
+	}
+
+}
