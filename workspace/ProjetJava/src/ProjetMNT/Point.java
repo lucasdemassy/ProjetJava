4b8a4fc41e7230ac1391b666b2fde1bc314d5592
lucasdemassy <38038514+lucasdemassy@users.noreply.github.com>
2018-04-10 14:32:18 +0200

    Add files via upload

--- workspace/ProjetJava/src/ProjetMNT/Point.java
+++ workspace/ProjetJava/src/ProjetMNT/Point.java
@@ -6,30 +6,58 @@ public class Point {
 	private double z;
 	
 	public Point(double x, double y, double z)	{
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
+	
+	public String toString(){
+		return "x= " + this.x + " y= " + this.y + " z= " + this.z + "\n";
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
 		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
 		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
 		this.z = z;
 	}
 	
 	public static double[] pointIntermediaire(Point point1, Point point2, double altitude)	{
-		if ((altitude <= point1.z & altitude >= point2.z) | (altitude <= point2.z & altitude >= point1.z))	{	
-			double distance = Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
-			double difference_altitude = Math.abs(point2.z - point1.z);
-			double X = 0;
-			double Y = 0;
-				if (point1.z < point2.z)	{
-					double difference_petite = altitude - point1.z;
-					double distance_petite = difference_petite * distance / difference_altitude;
-					double angle = Math.atan((point2.y - point1.y) / (point2.x - point1.x));
-					X = point1.x + (Math.cos(angle)*distance_petite); 
-					Y = point1.y + (Math.sin(angle)*distance_petite);
+		if ((altitude <= point1.getZ() & altitude >= point2.getZ()) | (altitude <= point2.getZ() & altitude >= point1.getZ()))	{	
+			double distance            = Math.sqrt(Math.pow(point2.getX() - point1.getX(), 2) + Math.pow(point2.getY() - point1.getY(), 2));
+			double difference_altitude = Math.abs(point2.getZ() - point1.getZ());
+			double X                   = 0;
+			double Y                   = 0;
+				if (point1.getZ() < point2.getZ())	{
+					double difference_petite = altitude - point1.getZ();
+					double distance_petite   = difference_petite * distance / difference_altitude;
+					double angle             = Math.atan((point2.getY() - point1.getY()) / (point2.getX() - point1.getX()));
+					X                        = point1.getX() + (Math.cos(angle)*distance_petite); 
+					Y                        = point1.getY() + (Math.sin(angle)*distance_petite);
 				}
 				else	{
-					double difference_petite = altitude - point2.z;
-					double distance_petite = difference_petite * distance / difference_altitude;
-					double angle = Math.atan((point1.y - point2.y) / (point1.x - point2.x));
-					X = point2.x + (Math.cos(angle)*distance_petite); 
-					Y = point2.y + (Math.sin(angle)*distance_petite);
+					double difference_petite = altitude - point2.getZ();
+					double distance_petite   = difference_petite * distance / difference_altitude;
+					double angle             = Math.atan((point1.getY() - point2.getY()) / (point1.getX() - point2.getX()));
+					X                        = point2.getX() + (Math.cos(angle)*distance_petite); 
+					Y                        = point2.getY() + (Math.sin(angle)*distance_petite);
 				}
 				double[] coordonnees = {X, Y, altitude};
 				return coordonnees;
@@ -42,13 +70,14 @@ public class Point {
 	}
 	
 	public static void main(String[] args) {
-		Point point1 = new Point(0,0,0);
-		Point point2 = new Point(0,1,5);
-		Point point3 = new Point(1,0,10);
+		Point point1   = new Point(0,0,0);
+		Point point2   = new Point(0,1,5);
+		Point point3   = new Point(1,0,10);
 		double[] value = pointIntermediaire(point1, point2, 1);
 		System.out.println(value[0] + ", " + value[1] + ", " + value[2]);
 		double[] value2 = pointIntermediaire(point2, point3, 4);		//Faire en sorte que les erreurs ne bloquent pas le code
 		System.out.println(value2[0] + ", " + value2[1] + ", " + value2[2]);
 	}
+
 
 }
--- workspace/ProjetJava/src/ProjetMNT/PointAleatoire.java
+++ workspace/ProjetJava/src/ProjetMNT/PointAleatoire.java
@@ -1,10 +1,33 @@
 package ProjetMNT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PointAleatoire extends Point{
 
 	public PointAleatoire(double x, double y, double z) {
 		super(x, y, z);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static List<Point> MNTAleatoire(double longueur, double largeur, double precision, double altitudeMin, double altitudeMax)	{
+		// Faire gaffe aux points entiers (éviter les points décimaux)
+		List<Point> grille = new ArrayList<Point>();
+		double altitude;
+		for (double i = 0; i <= largeur; i+=precision)	{
+			for (double j = 0; j <= longueur; j+=precision)	{
+				altitude = altitudeMin + Math.random()*(altitudeMax - altitudeMin);
+				grille.add(new PointAleatoire(i,j,altitude));
+			}
+		}
+		return grille;
+	}
+	
+	public static void main(String[] args) {
+		List<Point> MNT1 = MNTAleatoire(20,30,1,0,100);
+		for(int i=0; i<MNT1.size(); i++)	{
+			System.out.println(MNT1.get(i).getX() + ", " + MNT1.get(i).getY() + ", " + MNT1.get(i).getZ());
+		}
 	}
 
 }
