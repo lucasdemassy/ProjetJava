fe6d1899c7faba4fdf2f96584488daccf8d8e957
lucasdemassy <38038514+lucasdemassy@users.noreply.github.com>
2018-04-10 10:25:27 +0200

    Add files via upload

---
+++ ProjetJava/src/ProjetMNT/Point.java
@@ -0,0 +1,15 @@
+package ProjetMNT;
+
+public class Point {
+	private double x;
+	private double y;
+	private double z;
+	
+	public Point(double x, double y, double z)	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	
+	}
+
+}
